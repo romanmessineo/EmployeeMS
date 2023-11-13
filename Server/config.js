2e37fe720c41ba3917542223b2a0e5d22fd3fe7f
roman.messieo <messineoroman@gmail.com>
2023-11-12 23:20:49 -0300

    PORT

--- Server/config.js
+++ Server/config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const SERVER_PORT = process.env.SERVER_PORT || '3000';
+export const PORT = process.env.PORT || '3000';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ import { EmployeeRouter } from "./Routes/EmployeeRoute.js";
 import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import {
-    SERVER_PORT,
+    PORT,
     JWT_SECRET_KEY,
     CORS_ORIGIN,
 } from './config.js';
@@ -59,6 +59,6 @@ app.use((err, req, res, next) => {
     res.status(500).json({ Status: false, Error: 'Something went wrong!' });
 });
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on port ${SERVER_PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
