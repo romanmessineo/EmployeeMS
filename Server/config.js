710890acf9534402ac5818af28d80581ece707d1
roman.messieo <messineoroman@gmail.com>
2023-11-12 23:46:41 -0300

    pass mysql

--- Server/config.js
+++ Server/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const PORT = process.env.PORT || '3000';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root123';
 export const DB_NAME = process.env.DB_NAME || 'employeems';
 export const DB_PORT_NUMBER = process.env.DB_PORT_NUMBER || '3306';
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'your_default_secret_key';
